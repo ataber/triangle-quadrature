e0cfb2e16141f5cc48aab60544baf7920093fb4b
Andrew Taber <andrew.e.taber@gmail.com>
2017-11-20 20:42:15 -0800

    Fix example rendering.

--- example.js
+++ example.js
@@ -1,16 +1,26 @@
 var bunny = require('bunny');
 var mass = require('mesh-mass');
 var quadrature = require('./index.js');
+var colormap = require('colormap');
 var normals = require('normals');
 var surfaceArea = 0;
 var positions = [];
 var weights = [];
+var maximum = 1e-90;
+var minimum = 1e90;
 
 bunny.cells.map(function(cell) {
-  var rule = quadrature([bunny.positions[cell[0]], bunny.positions[cell[1]], bunny.positions[cell[2]]]);
+  var rule = quadrature([bunny.positions[cell[0]], bunny.positions[cell[1]], bunny.positions[cell[2]]], 5);
   rule.weights.map(function(weight) {
     surfaceArea += weight;
     weights.push(weight);
+    if (weight > maximum) {
+      maximum = weight;
+    }
+
+    if (weight < minimum) {
+      minimum = weight;
+    }
   });
 
   rule.positions.map(function(p) {
@@ -18,37 +28,53 @@ bunny.cells.map(function(cell) {
   });
 });
 
+var map = colormap({
+  colormap: 'jet',
+  nshades: 255,
+  format: 'rgb'
+})
+
+var colors = [];
+weights.map(function(weight) {
+  var index = Math.max(Math.min(parseInt(255 * ((weight - minimum) / (maximum - minimum))), 254), 0)
+  var value = map[index];
+  colors.push([value[0], value[1], value[2]]);
+})
+
 console.log("Quadrature area", surfaceArea);
 console.log("Actual area", mass(bunny.cells, bunny.positions).area);
 
 var regl = require('regl')();
 var mat4 = require('gl-mat4');
 var camera = require('regl-camera')(regl, {
-  center: [0, 0, 0],
+  center: [0, 5, 0],
   theta: Math.PI / 2,
-  distance: 4
+  distance: 15
 });
 
 const drawParticles = regl({
   vert: `
   precision mediump float;
-  attribute float weight;
+  attribute vec3 color;
+  varying vec3 vColor;
   attribute vec3 position;
   uniform mat4 view, projection;
   void main() {
-    gl_PointSize = 1.0;
-    gl_Position = projection * view * vec4(position, 1);
+    vColor = color;
+    gl_PointSize = 4.0;
+    gl_Position = projection * view * vec4(position, 1.0);
   }`,
   frag: `
   precision mediump float;
+  varying vec3 vColor;
   void main() {
-    gl_FragColor = vec4(0.75, 0.5, 0.3, 1);
+    gl_FragColor = vec4(vColor, 1.0);
   }`,
   attributes: {
-    weight: regl.prop("weights"),
-    position: regl.prop("positions")
+    color: colors,
+    position: positions
   },
-  count: regl.prop("positions").length,
+  count: positions.length,
   primitive: 'points'
 })
 
@@ -81,7 +107,8 @@ var drawOuter = regl({
   precision mediump float;
   varying vec3 vNorm;
   void main() {
-    gl_FragColor = vec4(vNorm, 1.0);
+    vec3 lightDir = normalize(vec3(1.0, 0.5, 0.));
+    gl_FragColor = vec4(vec3(0.6 * dot(vNorm, lightDir)), 1.0);
   }
   `,
   attributes: {
@@ -100,9 +127,7 @@ regl.frame(() => {
 
   camera(() => {
     drawParticles({
-      view: mat4.create(),
-      positions: positions,
-      weights: weights
+      view: mat4.create()
     })
 
     drawOuter({
